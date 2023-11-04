983e8441967d1722cd660bf4a7b52a50f1f3fd96
Klevert Opee <81738300+klevert-ope@users.noreply.github.com>
2023-11-05 00:35:59 +0300

    bug fix on all files and package change and addition to the background image generation

---
+++ components/sections/S2.jsx
@@ -0,0 +1,103 @@
+import CustomImage from "@/components/CustomImage";
+
+function S2() {
+    return (
+        <section id="S2parent">
+            <div id="S2div1">
+                <div>
+                    <blockquote>
+                        Join the journey to healthier, more beautiful skin. Explore our premium skincare line, and let
+                        Curology be your trusted companion in achieving the flawless skin you deserve.
+                    </blockquote>
+                    <blockquote>
+                        Experience the difference of Curology is carefully curated ingredients, expertly blended to
+                        nurture
+                        and transform your skin. It is time to embrace a radiant complexion and glow with confidence.
+                    </blockquote>
+                </div>
+            </div>
+            <div id="S2div2">
+                <div>
+                    <CustomImage
+                        src='images/curology-angela-roma%20ad.webp'
+                        alt='Curology Showcase Ad'
+                        width={667}
+                        height={311}
+                    />
+                    <figcaption>
+                        Indulge in the luxury of self-care with our products, designed to pamper and rejuvenate your
+                        skin. With Curology, you are not just buying skincare; you are investing in your confidence and
+                        well-being.
+                    </figcaption>
+                </div>
+            </div>
+            <div id="S2div3">
+                <div>
+                    <CustomImage
+                        src='images/curology-sink%20ad.webp'
+                        alt='Curology Showcase Ad'
+                        width={211}
+                        height={367}
+                    />
+                    <figcaption>
+                        <ul>Dermatologist-designed.</ul>
+                        <ul>Science-based.</ul>
+                        <ul>Made for your skin goals.</ul>
+                    </figcaption>
+                </div>
+            </div>
+            <div id="S2div4">
+                <div>
+                    <CustomImage
+                        src='images/curology-poster.webp'
+                        alt='Curology Showcase Ad'
+                        width={311}
+                        height={467}
+                    />
+                </div>
+            </div>
+            <div id="S2div5">
+                <div>
+                    <CustomImage
+                        src='images/curology-granny%20ad.webp'
+                        alt='Curology Showcase Ad'
+                        width={311}
+                        height={667}
+                    />
+                    <figcaption>
+                        <ul>Dermatologist-designed.</ul>
+                        <ul>Science-based.</ul>
+                        <ul>Made for your skin goals.</ul>
+                    </figcaption>
+                </div>
+            </div>
+            <div id="S2div6">
+                <div>
+                    <CustomImage
+                        src='images/curology-showcase%20ad.webp'
+                        alt='Curology Showcase Ad'
+                        width={412}
+                        height={388}
+                    />
+                    <blockquote>
+                        Experience the difference of Curology is carefully curated ingredients, expertly blended to
+                        nurture
+                        and transform your skin. It is time to embrace a radiant complexion and glow with confidence.
+                    </blockquote>
+                </div>
+            </div>
+            <div id="S2div7">
+                <div>
+                    <CustomImage
+                        src='images/curology-angela-roma%20ad.webp'
+                        alt='Curology Showcase Ad'
+                        width={667}
+                        height={311}
+                    />
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default S2;
