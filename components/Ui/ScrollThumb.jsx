983c782a67d82fde5b8e53943e54b249c9e8cca8
Klevert Opee <81738300+klevert-ope@users.noreply.github.com>
2023-11-11 13:45:16 +0300

    bug fixes

---
+++ components/Ui/ScrollThumb.jsx
@@ -0,0 +1,24 @@
+import {useScrollThumb} from '@/hooks/Ui/useScrollThumb';
+import {useMemo} from 'react';
+
+function ScrollThumb() {
+	const {scrollPercentage, showThumb, thumbRef} = useScrollThumb();
+
+	return useMemo(() => {
+		const thumbStyle = {
+			position: 'fixed',
+			right: '4px',
+			top: `${scrollPercentage}%`,
+			opacity: showThumb ? 1 : 0,
+			transition: 'opacity 0.5s',
+		};
+
+		return (
+			<div style={thumbStyle} ref={thumbRef}>
+				<div className='z-50 my-1 h-16 rounded-lg bg-amber-300 w-1.5'/>
+			</div>
+		);
+	}, [scrollPercentage, showThumb, thumbRef]);
+}
+
+export default ScrollThumb;
