96785a5be745097c6c843e5871c0103f5528d128
Klevert Opee <81738300+klevert-ope@users.noreply.github.com>
2023-11-11 13:44:52 +0300

    image performance improvements and new background image

---
+++ hooks/Images/useImageFetcher.jsx
@@ -0,0 +1,18 @@
+import {fetchImage} from '@/api/FetchImage';
+import {useQuery} from 'react-query';
+
+export function useImageFetcher(src) {
+	const queryKey = ['image', src];
+
+	const {
+		data: imageUrl,
+		isLoading,
+		error,
+	} = useQuery(queryKey, () => fetchImage(src), {enabled: !!src,});
+
+	return {
+		imageUrl,
+		isLoading,
+		error,
+	};
+}
