73bb3a08f9997335875a8c8d6e7daceee2c96b9e
Klevert Opee <81738300+klevert-ope@users.noreply.github.com>
2023-11-19 22:36:30 +0300

    package and package manager changes

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'essmnbcneybekiriuadi.supabase.co',
-				port: '',
-			},
-		],
-	},
-	env: {
-		SUPABASE_KEY: process.env.SUPABASE_KEY,
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'essmnbcneybekiriuadi.supabase.co',
+        port: ''
+      }
+    ]
+  },
+  env: {
+    SUPABASE_KEY: process.env.SUPABASE_KEY
+  }
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
